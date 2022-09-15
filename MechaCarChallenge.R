0eb99d5f293c07d60f70c0a080d5d9fb17e5951b
Kelley Richards <106293233+clevkelz@users.noreply.github.com>
2022-09-15 13:33:55 -0400

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -6,3 +6,8 @@ summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,
 SuspensionCoil_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
 total_summary <- SuspensionCoil_table %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
 lot_summary <- SuspensionCoil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+
+t.test(SuspensionCoil_table$PSI, mu=1500)
+t.test(subset(SuspensionCoil_table,Manufacturing_Lot=="Lot1")$PSI, mu = 1500)
+t.test(subset(SuspensionCoil_table,Manufacturing_Lot=="Lot2")$PSI, mu = 1500)
+t.test(subset(SuspensionCoil_table,Manufacturing_Lot=="Lot3")$PSI, mu = 1500)
